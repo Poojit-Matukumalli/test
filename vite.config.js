b2552f13edd57ea6fae7ce55cb7e09b233d8e7be
Poojit-Matukumalli <poojit.matukumalli@gmail.com>
2026-05-29 19:04:58 +0000

    vite test build

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: './src/index.html',
+        about: './src/about.html',
+        contact: './src/contact.html',
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+});
